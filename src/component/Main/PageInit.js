a49f9d98110e37eac638a9bcace21d90c2c37c07
MdGolamMostafa <shakilhero11@gmail.com>
2020-12-13 22:42:30 +0600

    start new project

---
+++ src/component/Main/PageInit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageInit = () => {
+    return (
+        <div>
+            <h1>Bismillah PageInit</h1>
+        </div>
+    );
+};
+
+export default PageInit;
---
+++ src/component/Main/Records.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Records = () => {
+    return (
+        <div>
+            <h1>Bismillah Records</h1>
+        </div>
+    );
+};
+
+export default Records;
